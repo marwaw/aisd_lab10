8dfd1b336b7f5cfcefb11080e38c4d74262b7dfb
marwaw <martyna.waw96@gmail.com>
2016-05-15 13:12:28 +0200

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Huffman h = new Huffman();
-		h.addToPriority(h.read("myText.txt"));
-		h.display(h.createHuffmanTree(), "");
-		
+		Huffman h = new Huffman("myText.txt");
+//		h.display(h.huffmanTree, "");
+//		h.codeText(h.huffmanTree, s);
+//		h.symbolBook.inOrder(h.symbolBook.root);
 		
 //		System.out.println(h.search('a'));
 
